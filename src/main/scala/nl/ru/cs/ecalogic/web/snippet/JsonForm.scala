71812759df9a6f65f76c12e9d9d17314d11eea76
Dorus Peelen <dpeelen@gmail.com>
2013-11-18 17:17:20 +0100

    output newlines in errors.

--- src/main/scala/nl/ru/cs/ecalogic/web/snippet/JsonForm.scala
+++ src/main/scala/nl/ru/cs/ecalogic/web/snippet/JsonForm.scala
@@ -52,7 +52,7 @@ import java.io.PrintWriter
 object JsonForm {
 
   def render =
-    "#jsonForm" #> ((ns:NodeSeq) => jsonForm(AnalyseServer, ns)) &
+    "#jsonForm" #> ((ns: NodeSeq) => jsonForm(AnalyseServer, ns)) &
       "#jsonScript" #> Script(AnalyseServer.jsCmd)
 
   object AnalyseServer extends JsonHandler {
@@ -60,34 +60,34 @@ object JsonForm {
       case JsonCmd("processForm", target, params: Map[String, String], all) =>
         val code = params.getOrElse("code", "")
 
-      val baos = new ByteArrayOutputStream()
-      val pw = new PrintWriter(baos)
+        val baos = new ByteArrayOutputStream()
+        val pw = new PrintWriter(baos)
         val errorHandler = new DefaultErrorHandler(source = Some(code), writer = pw)
         val parser = new Parser(code, errorHandler)
         val program = parser.program()
-       if (errorHandler.errorOccurred) {
-         return SetHtml("result", Text("Parse error: %s".format(baos.toString)))
-       }
+        if (errorHandler.errorOccurred) {
+          // TODO is this safe?
+          return SetHtml("result", scala.xml.Unparsed("Parse error: %s".format(baos.toString).replace("\n", "<br>")))
+        }
 
         val checker = new SemanticAnalysis(program, errorHandler)
         checker.functionCallHygiene()
         checker.variableReferenceHygiene()
         if (errorHandler.errorOccurred) {
-          return SetHtml("result", Text("Semantic error: %s".format(baos.toString)))
+          return SetHtml("result", scala.xml.Unparsed("Semantic error: %s".format(baos.toString)))
         }
 
-        val components = Set(StubComponent, BadComponent, Sensor, Radio, if(config.Options.noCPU) StubComponent else CPU)
+        val components = Set(StubComponent, BadComponent, Sensor, Radio, if (config.Options.noCPU) StubComponent else CPU)
 
         val consumptionAnalyser = new EnergyAnalysis(program, components, errorHandler)
 
         if (errorHandler.errorOccurred) {
           // TODO do we need this?
-          return SetHtml("result", Text("Analyse error: %s".format(baos.toString)))
+          return SetHtml("result", scala.xml.Unparsed("Analyse error: %s".format(baos.toString)))
         }
 
-        SetHtml("result",
-          Text("The code is %s/nThe error is %s".format(consumptionAnalyser().toString,
-            baos.toString)) )
+        SetHtml("result", scala.xml.Unparsed("The code is %s".format(consumptionAnalyser().toString)))
     }
   }
+
 }
