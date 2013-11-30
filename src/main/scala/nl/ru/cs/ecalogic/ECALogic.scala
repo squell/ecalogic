c0c1e834d15205f22314cdfc6834d1afc1f93cc8
Jascha <JaschaNeutelings@student.ru.nl>
2013-11-30 23:17:12 +0100

    Removed Stub.ecm from examples (it is also in resources) Gave run class a slightly less horrible name

--- src/main/scala/nl/ru/cs/ecalogic/ECALogic.scala
+++ src/main/scala/nl/ru/cs/ecalogic/ECALogic.scala
@@ -43,50 +43,48 @@ import java.io.File
 
 import model.ComponentModel
 
-object run {
+object ECALogic extends App {
 
-  def main(args: Array[String]) {
-    val fileName = Options(args).headOption.getOrElse("program.eca")
-    val defaultErrorHandler = new DefaultErrorHandler
-    try {
-      val file = new File(fileName)
-      val source = defaultErrorHandler.handle(Source.fromFile(file).mkString)
-      val errorHandler = new DefaultErrorHandler(sourceText = Some(source), sourceURI = Some(file.toURI))
+  val fileName = Options(args).headOption.getOrElse("program.eca")
+  val defaultErrorHandler = new DefaultErrorHandler
+  try {
+    val file = new File(fileName)
+    val source = defaultErrorHandler.handle(Source.fromFile(file).mkString)
+    val errorHandler = new DefaultErrorHandler(sourceText = Some(source), sourceURI = Some(file.toURI))
 
-      val program = errorHandler.handleBlock("One or more errors occurred during parsing. Aborted.") {
-        val parser = new Parser(source, errorHandler)
-        parser.program()
-      }
-
-      errorHandler.handleBlock("One or more errors occurred during semantic analysis. Aborted.") {
-        val checker = new SemanticAnalysis(program, errorHandler)
-        checker.functionCallHygiene()
-        checker.variableReferenceHygiene()
-      }
-
-      //val stub = ECMModel.fromFile("doc/examples/Stub.ecm")
-      //val components = Set[ComponentModel](stub)
-      //val components = Set(StubComponent, BadComponent, Sensor, Radio, if(config.Options.noCPU) StubComponent else CPU)
-
-      val components = errorHandler.handleBlock("One or more errors occurred while loading components. Aborted.") {
-        ComponentModel.fromImports(program.imports, errorHandler)
-      }
-
-      val finalState = errorHandler.handleBlock("One or more errors occurred during energy analysis. Aborted.") {
-        val consumptionAnalyser = new EnergyAnalysis(program, components, errorHandler)
-        consumptionAnalyser.analyse()
-      }
-      println(finalState)
-
-      println(s"Analysis of '${file.getAbsolutePath}' was successful.")
-    } catch {
-      case _: ECAException          =>
-        sys.exit(1)
-      case e: Exception =>
-        Console.err.println("Oops. An exception seems to have escaped.")
-        e.printStackTrace()
-        sys.exit(2)
+    val program = errorHandler.handleBlock("One or more errors occurred during parsing. Aborted.") {
+      val parser = new Parser(source, errorHandler)
+      parser.program()
     }
+
+    errorHandler.handleBlock("One or more errors occurred during semantic analysis. Aborted.") {
+      val checker = new SemanticAnalysis(program, errorHandler)
+      checker.functionCallHygiene()
+      checker.variableReferenceHygiene()
+    }
+
+    //val stub = ECMModel.fromFile("doc/examples/Stub.ecm")
+    //val components = Set[ComponentModel](stub)
+    //val components = Set(StubComponent, BadComponent, Sensor, Radio, if(config.Options.noCPU) StubComponent else CPU)
+
+    val components = errorHandler.handleBlock("One or more errors occurred while loading components. Aborted.") {
+      ComponentModel.fromImports(program.imports, errorHandler)
+    }
+
+    val finalState = errorHandler.handleBlock("One or more errors occurred during energy analysis. Aborted.") {
+      val consumptionAnalyser = new EnergyAnalysis(program, components, errorHandler)
+      consumptionAnalyser.analyse()
+    }
+    println(finalState)
+
+    println(s"Analysis of '${file.getAbsolutePath}' was successful.")
+  } catch {
+    case _: ECAException =>
+      sys.exit(1)
+    case e: Exception =>
+      Console.err.println("Oops. An exception seems to have escaped.")
+      e.printStackTrace()
+      sys.exit(2)
   }
 
 }
