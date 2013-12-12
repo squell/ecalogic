26ef3dc07682ac93b0b82eface67e4523074e053
Marc Schoolderman <mschool@science.ru.nl>
2013-12-12 15:10:14 +0100

    silenced warning

--- src/main/scala/nl/ru/cs/ecalogic/interpreter/BaseInterpreter.scala
+++ src/main/scala/nl/ru/cs/ecalogic/interpreter/BaseInterpreter.scala
@@ -96,6 +96,8 @@ trait BaseInterpreter {
       statements.foldLeft(state) {
         case (state, stmt) => evalStatement(stmt, state, stackTrace)
       }
+    case ErrorNode() =>
+      throw new Exception("This can never happen.")
   }
 
   protected def evalExprList(exprs: Seq[Expression], state: IState, stackTrace: StackTraceBuilder) =
