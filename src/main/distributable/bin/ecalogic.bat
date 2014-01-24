40b99f9f6fa45ebf6ce7614b749aeb21f2234bc0
Jascha <JaschaNeutelings@student.ru.nl>
2014-01-24 23:49:11 +0100

    Launch scripts now use -jar flag instead of explicitly stating the main class

--- src/main/distributable/bin/ecalogic.bat
+++ src/main/distributable/bin/ecalogic.bat
@@ -21,7 +21,7 @@ if "%_JAVACMD%"=="" (
 
 if "%_JAVACMD%"=="" set _JAVACMD=java
 
-"%_JAVACMD%" %JAVA_OPTS% "-Decalogic.home=%ECALOGIC_HOME%" -cp "%ECALOGIC_HOME%\lib\ecalogic.jar" nl.ru.cs.ecalogic.ECALogic %*
+"%_JAVACMD%" %JAVA_OPTS% "-Decalogic.home=%ECALOGIC_HOME%" -jar "%ECALOGIC_HOME%\lib\ecalogic.jar" %*
 @endlocal
 
 @%COMSPEC% /C exit %errorlevel% > nul
