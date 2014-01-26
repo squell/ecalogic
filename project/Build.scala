0fb85118ba5d17bccc2b2d36fa05befdbff5e0ed
Jascha <JaschaNeutelings@student.ru.nl>
2014-01-26 16:58:58 +0100

    Prepared for release version 0.1

--- project/Build.scala
+++ project/Build.scala
@@ -55,7 +55,7 @@ object Build extends sbt.Build {
 
   override lazy val settings = super.settings ++ Seq (
     organization   := "nl.ru.cs.ecalogic",
-    version        := "0.1-SNAPSHOT",
+    version        := "0.1",
     scalaVersion   := "2.10.3",
     crossPaths     := false,
     scalacOptions ++= Seq("-deprecation", "-unchecked", "-encoding", "UTF8")
